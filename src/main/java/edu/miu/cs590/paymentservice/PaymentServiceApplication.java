3253c39a9efe9c8b81a8086b8d7ac7882d851f4a
sourav40 <souravshrestha40@gmail.com>
2022-09-27 19:04:41 -0500

    jenkins issue tracking.

--- src/main/java/edu/miu/cs590/paymentservice/PaymentServiceApplication.java
+++ src/main/java/edu/miu/cs590/paymentservice/PaymentServiceApplication.java
@@ -15,7 +15,6 @@ public class PaymentServiceApplication {
     }
 
     public static void main(String[] args) {
-        System.out.println("starting application service ...");
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
